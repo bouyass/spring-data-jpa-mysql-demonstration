dab902673700ea01d589d74d78b14f51bf8a8e35
lyes <lyes.makhloufi@outlook.fr>
2020-07-04 23:06:18 +0200

    all student method implementation

--- src/main/java/com/lyes/controllers/StudentController.java
+++ src/main/java/com/lyes/controllers/StudentController.java
@@ -2,6 +2,7 @@ package com.lyes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,21 +12,26 @@ import com.lyes.entities.Student;
 import com.lyes.services.StudentRepo;
 
 @Controller
-@RequestMapping(path="/student")
+@RequestMapping(path = "/student")
 public class StudentController {
-	
+
 	@Autowired
 	private StudentRepo sr;
-	
-	@PostMapping(path="/add")
-	public @ResponseBody String addNewStudent(@RequestParam String name,@RequestParam int age) {
-		Student student = new Student(name,age);
-		sr.save(student);
-		return "saved";
+
+	@PostMapping(path = "/add")
+	public @ResponseBody String addNewStudent(@RequestParam String name, @RequestParam int age) {
+		if (!name.equals("") && age != 0) {
+			Student student = new Student(name, age);
+			sr.save(student);
+			return "saved";
+		} else {
+			return "missing paramaters";
+		}
 	}
-	
-	public @ResponseBody Iterable<Student> getAllStudents(){
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<Student> getAllStudents() {
 		return sr.findAll();
 	}
-	
+
 }
